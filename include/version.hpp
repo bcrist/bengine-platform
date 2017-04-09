3d851a55dba891bed6ec5101b288d8e8513aa347
Ben Crist <ben@magicmoremagic.com>
2017-04-09 18:17:39 -0500

    versioning

---
+++ include/version.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef BE_PLATFORM_VERSION_HPP_
+#define BE_PLATFORM_VERSION_HPP_
+
+#include <be/core/macros.hpp>
+
+#define BE_PLATFORM_VERSION_MAJOR 0
+#define BE_PLATFORM_VERSION_MINOR 1
+#define BE_PLATFORM_VERSION_REV 1
+
+/*!! include('common/version', 'be::platform') !! 6 */
+/* ################# !! GENERATED CODE -- DO NOT MODIFY !! ################# */
+#define BE_PLATFORM_VERSION (BE_PLATFORM_VERSION_MAJOR * 100000 + BE_PLATFORM_VERSION_MINOR * 1000 + BE_PLATFORM_VERSION_REV)
+#define BE_PLATFORM_VERSION_STRING "be::platform " BE_STRINGIFY(BE_PLATFORM_VERSION_MAJOR) "." BE_STRINGIFY(BE_PLATFORM_VERSION_MINOR) "." BE_STRINGIFY(BE_PLATFORM_VERSION_REV)
+
+/* ######################### END OF GENERATED CODE ######################### */
+
+#endif
